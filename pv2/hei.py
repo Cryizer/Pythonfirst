e369cf023fc6463e67494cb41dcf4423fbdf2efc
Cryizer <cryizer@hotmail.fi>
2021-05-25 14:14:50 +0300

    Lomake

--- pv2/hei.py
+++ pv2/hei.py
@@ -12,4 +12,13 @@ def kek():
 	animals = ["lion", "tiger", "rabbit", "racoon"]
 	return render_template("kek.html", name="Krister", animals=animals)
 
+@app.route("/foo", methods=["GET", "POST"])
+def foo():
+        return render_template("foo.html", name="Krister")
+
+@app.route("/get", methods=["GET", "POST"])
+def get():
+        return render_template("get.html", name="Krister")
+
+
 app.run()
