8630f15ad8ad9295ace8f616c68352d54d13b03c
Cryizer <cryizer@hotmail.fi>
2021-05-25 15:47:23 +0300

    name change

--- pv2/hei.py
+++ pv2/hei.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	return render_template("base.html", name="Krister")
+	return render_template("base.html", name="Coder")
 
-@app.route("/kek")
-def kek():
+@app.route("/loop")
+def loop():
 	animals = ["lion", "tiger", "rabbit", "racoon"]
-	return render_template("kek.html", name="Krister", animals=animals)
+	return render_template("loop.html", name="Krister", animals=animals)
 
-@app.route("/foo", methods=["GET", "POST"])
-def foo():
-        return render_template("foo.html", name="Krister")
+@app.route("/post", methods=["GET", "POST"])
+def post():
+        return render_template("post.html", name="Visitor")
 
 @app.route("/get", methods=["GET", "POST"])
 def get():
-        return render_template("get.html", name="Krister")
+        return render_template("get.html", name="Visitor")
+
+@app.route("/date", methods=["GET", "POST"])
+def date():
+        return render_template("date.html", name="Visitor")
 
 
 app.run()
