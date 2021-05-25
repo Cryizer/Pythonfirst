73ec5613071d216e0ede6124f71b645c2ca85c6a
Cryizer <cryizer@hotmail.fi>
2021-05-25 11:50:06 +0300

    if/else

--- pv2/hei.py
+++ pv2/hei.py
@@ -5,10 +5,11 @@ app=Flask(__name__)
 @app.route("/")
 def index():
 	animals = ["lion", "tiger", "rabbit", "racoon"]
-	return render_template("base.html", name="Krister",  animals=animals)
+	return render_template("base.html", name="Krister", animals=animals)
 
 @app.route("/kek")
 def kek():
-        return render_template("kek.html", name="Krister")
+	animals = ["lion", "tiger", "rabbit", "racoon"]
+	return render_template("kek.html", name="Krister", animals=animals)
 
 app.run()
